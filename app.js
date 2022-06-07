2c90790ea93ff655b4fa70f68d18d8848bb02128
bryanmsombra27 <bryanjm96@gmail.com>
2022-06-06 23:27:32 -0500

    inicio del proyecto

---
+++ app.js
@@ -0,0 +1,7 @@
+const tareasPendientes = document.querySelector('#pendientes');
+const tareasExitosas = document.querySelector('#exitosas');
+const enviarTarea = document.querySelector('#enviar');
+let tarea = document.querySelector('#tarea');
+
+
+
